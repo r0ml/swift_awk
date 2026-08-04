c0ffa47d7cab0d390239019d7634f3c5b167cc0c
Robert Lefkowitz <r0ml@mac.com>
2026-08-04 01:51:34 -0400

    ready to test 34 cases

--- Tests/awkTest.swift
+++ Tests/awkTest.swift
@@ -4,15 +4,43 @@
 
 import ShellTesting
 
-struct awkTest : ShellTest {
+@Suite("awkTest", .serialized) struct awkTest : ShellTest {
   var cmd = "awk"
   var suiteBundle = "awk_awkTest"
 
-  @Test("Checks basic functionality") func basic() async throws {
-    let expected = try fileContents("expected.p.1")
-    let inp1 = try inFile("p.1")
-    let inp2 = try inFile("test.countries")
+  static func getList() -> [String] {
+    let j = try? awkTest().geturl()
+    let k = try? j!.listDirectory().sorted()
 
-    try await run(output: expected, args: "-f", inp1, inp2)
+    let k2 = k!.filter { $0.hasPrefix("expected.") }
+    return k2
+  }
+
+  @Test("Checks basic functionality",
+        arguments: getList()[0..<34]
+        //        arguments: ["expected.p.1"]
+  ) func basic(_ f : String) async throws {
+/*    let j = try geturl()
+    let k = try j.listDirectory().sorted()
+
+    let k2 = k.filter { $0.hasPrefix("expected.") }
+*/
+//    for f in k2 {
+      print(f)
+    let g = String(f.dropFirst("expected.".count))
+      let expected = try fileContents(f)
+      let inp1 = try inFile(g)
+
+    if g.first == "t" {
+      let inp2 = try inFile("test.data")
+      try await run(output: expected, args: "-f", inp1, inp2)
+    } else {
+      let inp2 = try inFile("test.countries")
+      try await run(output: expected, args: "-f", inp1, inp2, inp2)
+
+    }
+
+    
+//    }
   }
 }
