499bda0b88af66fd2c91d19006513f30aaab93d6
Robert Lefkowitz <r0ml@mac.com>
2026-08-15 19:31:42 -0400

    T.exprconv

--- Tests/T.expr.swift
+++ Tests/T.expr.swift
@@ -9,14 +9,14 @@ extension awkTest {
 
 
     @Test("first") func first() async throws {
-      let foo1 = try tmpfile("foo1")
-      let foo2 = try tmpfile("foo2")
       let input = try inFile("T.expr.input")
       let prog = try inFile("T.expr.prog")
 
       try await run(withStdin: input,
                     output: "114 tests\n",
                     args: "-f", prog)
+      // FIXME: the prog here creates files foo1 and foo2
+      // which I need to delete
     }
   }
 }
--- Tests/T.exprconv.swift
+++ Tests/T.exprconv.swift
@@ -9,6 +9,30 @@ extension awkTest {
 
 
     @Test("first") func first() async throws {
+      let prog = """
+        BEGIN {  x = (1 > 0); print x
+          x = (1 < 0); print x
+          x = (1 == 1); print x
+          print ("a" >= "b")
+          print ("b" >= "a")
+          print (0 == 0.0)
+          # x = ((1 == 1e0) && (1 == 10e-1) && (1 == .1e2)); print x
+          exit
+        }
+        
+        """
+      let output = """
+        1
+        0
+        1
+        0
+        1
+        1
+        
+        """
+      
+      try await run(output: output,
+                    args: prog)
     }
   }
 }
